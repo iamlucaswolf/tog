0a174484a1523beb424518643c9aa8ab25ba562d
Lucas Wolf <iamlucaswolf@gmail.com>
2021-11-13 12:42:18 +0100

    Implement init command
    Add tomlplusplus, CLI11 as header-only libraries (mode of import
    may change in future commits).

--- src/tog.cpp
+++ src/tog.cpp
@@ -1,5 +1,56 @@
+#include <CLI11/CLI11.h>
+#include <tomlplusplus/toml.h>
+
+#include <filesystem>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+namespace fs = std::filesystem;
+
+void init() {
+    auto togdir_path = fs::current_path() / ".tog";
+
+    // Check if .tog directory exists
+    if (fs::exists(togdir_path)) {
+        std::cout << "Already a tog repository" << std::endl;
+        return;
+    }
+
+    // Create repository directories
+    fs::create_directories(togdir_path / "objects");
+    fs::create_directories(togdir_path / "refs" / "branches");
+
+    // Create default config file
+    auto config = toml::table{{
+        {"version", "0.0.0-alpha"},
+        {"worktree", ".."},
+    }};
+
+    std::ofstream config_file{togdir_path / "config.toml"};
+    config_file << config << std::endl;
+
+    // Create (empty) main branch
+    std::ofstream{togdir_path / "refs" / "branches" / "main"};
+
+    // Create head (pointing to main)
+    std::ofstream head_file{togdir_path / "head.toml"};
+    head_file << toml::table{{{"branch", "main"}}} << std::endl;
+
+    std::cout << "Initialized tog repository" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    CLI::App app{"A simple version control system", "tog"};
+    app.require_subcommand(1);
+
+    app.add_subcommand("init",
+                       "Creates a new repository in the current directory")
+        ->callback(init);
+
+    try {
+        CLI11_PARSE(app, argc, argv);
+    } catch (const CLI::ParseError &e) {
+        return app.exit(e);
+    }
+
+    return 0;
 }
